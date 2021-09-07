1b3e826d22a58f91ed41ac6e3b19872d0ffd06dc
hericlesLima <hericlesrecheado@gmail.com>
2021-09-07 11:35:03 -0100

    initial commit

---
+++ src/pages/Create.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+export default function Create() {
+  return (
+    <div>
+      <Typography 
+      variant='h1'>
+        Create a New Note
+      </Typography>
+    </div>
+  )
+}
